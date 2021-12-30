dd20717770213165ca0ccaef501202fdab6f8b2e
Kacper <pietrasikk@gmail.com>
2021-12-30 18:55:09 +0100

    Initial project

---
+++ src/main/java/com/manning/tutorial/notification/templateformatterservice/TemplateFormatterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.templateformatterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateFormatterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateFormatterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manning/tutorial/notification/templateformatterservice/TemplateFormatterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.templateformatterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateFormatterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
