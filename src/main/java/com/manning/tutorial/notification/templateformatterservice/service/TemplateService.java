ceafc736f5d69ea3be8a061ea600b09e14ff43c3
Kacper Pietrasik <pietrasikk@gmail.com>
2022-03-27 19:46:53 +0200

    Added Sleuth, Metrics

--- src/main/java/com/manning/tutorial/notification/templateformatterservice/service/TemplateService.java
+++ src/main/java/com/manning/tutorial/notification/templateformatterservice/service/TemplateService.java
@@ -5,14 +5,18 @@ import com.manning.tutorial.notification.templateformatterservice.model.Template
 import com.manning.tutorial.notification.templateformatterservice.service.formatter.EmailFormatter;
 import com.manning.tutorial.notification.templateformatterservice.service.formatter.SmsFormatter;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequiredArgsConstructor
 public class TemplateService {
 
     private final EmailFormatter emailFormatter;
     private final SmsFormatter smsFormatter;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public TemplateResponse format(TemplateRequest templateRequest) {
+        logger.info("In the Notification Template Formatter API Class");
         switch (templateRequest.getNotificationMode()) {
             case "EMAIL":
                 return emailFormatter.format(templateRequest);
